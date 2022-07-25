eb1ecd89626e23f831df063fbc363c8045b3bc70
Radouane Bahi <radbahi@gmail.com>
2022-07-25 11:16:44 -0400

    initial commit and add root route

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+#   how to send directly to a method. #methodname
+  root 'pages#home'
+
+end
